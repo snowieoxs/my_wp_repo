b07e7683ad1c066550b3ab7243a30f68c13dd460
snowie oxs <snowieoxs@gmail.com>
2024-08-20 23:54:47 -0300

    lil adjust to nginx.conf.template.script1.sh

--- nginx/nginx.conf.template.script1.sh
+++ nginx/nginx.conf.template.script1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Exit if the target configuration file does not exist
+if [ ! -f /etc/nginx/nginx.conf ]; then
+    echo "/etc/nginx/nginx.conf does not exist. Exiting..."
+    exit 1
+fi
+
+# Create a backup of the current configuration with a timestamp
+timestamp=$(date +"%Y%m%d%H%M%S")
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak_$timestamp
+
 # Define your variables
 ulimit_n=$(ulimit -n)
 num_cores=$(grep -c processor /proc/cpuinfo)
@@ -21,26 +31,17 @@ export GZIP_PROXIED="any"
 export GZIP_COMP_LEVEL="5"
 export GZIP_TYPES="text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript"
 
-# Check if the Nginx configuration file exists
-if [ ! -f /etc/nginx/nginx.conf ]; then
-    echo "Error: /etc/nginx/nginx.conf does not exist."
-    exit 1
-fi
-
-# Create a timestamp for the backup
-timestamp=$(date +"%Y%m%d%H%M%S")
-
-# Backup the existing Nginx configuration file
-cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak_$timestamp
-
 # Replace variables in the template file and copy to destination
-envsubst < ./nginx.template1.conf > /etc/nginx/nginx.conf
+envsubst < ./nginx.template1.conf | sudo tee /etc/nginx/nginx.conf > /dev/null
 
-# Inform the user
 echo "Backup created and new configuration applied."
 
-# test it
+# Test the new configuration
 sudo nginx -t
 
-# restart it
-service nginx restart
+# If the test is successful, reload Nginx
+if [ $? -eq 0 ]; then
+    sudo systemctl restart nginx
+else
+    echo "Nginx configuration test failed. Not restarting Nginx."
+fi
