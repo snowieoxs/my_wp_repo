83471d6fa43fbe2a285fa1c560df6249aa901e8d
snowie oxs <snowieoxs@gmail.com>
2024-08-23 18:44:06 -0300

    split up obtains-ssl-cert.sh

--- add-wp-site/config.sh
+++ add-wp-site/config.sh
@@ -8,6 +8,7 @@ export TLD="xyz" # This is the top level domain for the site
 export TITLE="Seeing Spinning Spheres Corporation" # This
 export WP_EMAIL="bbrootbeer@gmail.com"
 #####################################################
+export USERNAME="$USER" # Use the current user's username
 export WWWCNAME="www.$CNAME"
 export SUB_DOMAIN="$CNAME.$DOMAIN.$TLD"
 export WWW_SUB_DOMAIN="$WWWCNAME.$DOMAIN.$TLD"
